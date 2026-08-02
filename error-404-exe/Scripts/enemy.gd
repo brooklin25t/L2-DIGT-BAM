ccf59378b7d03a815fa7cd30ee08a566eaab0bfc
brooklin25t <brooklin25t@gisboyshigh.net>
2026-08-03 09:53:23 +1200

    backup-1

---
+++ error-404-exe/Scripts/enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+@export var player : Node2D
+
+func _physics_process(delta: float) -> void:
+	if player == null:
+		return
+	#Where is the player
+	var direction = (player.position - position).normalized()
+	velocity = direction * SPEED
+	move_and_slide()
+			
+	# Loop through all solid contacts from move_and_slide()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		
+		if collider.is_in_group("enemies"):
+			player.respawn()
