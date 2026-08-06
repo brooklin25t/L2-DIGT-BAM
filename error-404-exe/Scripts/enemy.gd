a3246944d12cf177251dc1fdfbc294bc8472cefe
brooklin25t <brooklin25t@gisboyshigh.net>
2026-08-06 14:30:04 +1200

    Enemy Respawn

--- error-404-exe/Scripts/enemy.gd
+++ error-404-exe/Scripts/enemy.gd
@@ -3,6 +3,14 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 @export var player : Node2D
+@onready var spawn_position: Vector2 = global_position
+
+func respawn():
+	# Reset the player's position back to the spawn
+	global_position = spawn_position
+	# Optional: Reset velocity to zero so the player doesn't keep falling/sliding
+	velocity = Vector2.ZERO 
+
 
 func _physics_process(delta: float) -> void:
 	if player == null:
@@ -19,3 +27,8 @@ func _physics_process(delta: float) -> void:
 		
 		if collider.is_in_group("enemies"):
 			player.respawn()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Player"):
+		respawn()
