019475077a8690cae7913e064e2074c92795950b
myles23s-gif <myles23s@gisboyshigh.net>
2026-08-17 10:28:55 +1200

    got it to work finally

--- error-404-exe/Scripts/enemy.gd
+++ error-404-exe/Scripts/enemy.gd
@@ -3,34 +3,33 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 @onready var player_prefab = preload("res://Prefab/player.tscn")
-#@onready var spawn_position: Vector2 = global_position
+@onready var spawn_position: Vector2 = global_position
 @onready var nav2d: NavigationAgent2D = $NavigationAgent2D
+var player: CharacterBody2D
 
+func respawn():
+	#Reset the player's position back to the spawn
+	global_position = spawn_position
+	#Optional: Reset velocity to zero so the player doesn't keep falling/sliding
+	velocity = Vector2.ZERO 
 
-#func respawn():
-	# Reset the player's position back to the spawn
-	#global_position = spawn_position
-	# Optional: Reset velocity to zero so the player doesn't keep falling/sliding
-	#velocity = Vector2.ZERO 
-	
-	
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("Target")
 
 
 func _physics_process(delta: float) -> void:
-	var player = player_prefab.instantiate()
-	var player_position: Vector2 = player.global_position
 	if not player:
 		print("help")
 		return
 	nav2d.target_position = player.global_position
 	if nav2d.is_navigation_finished():
 		velocity = Vector2.ZERO
-		return
-	var current_agent_position: Vector2 = player.global_position
+		move_and_slide()
+	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = nav2d.get_next_path_position()
 	var direction: Vector2 = current_agent_position.direction_to(next_path_position)
 	velocity = direction * SPEED
-	
+	move_and_slide()
 	
 	# Loop through all solid contacts from move_and_slide()
 	for i in get_slide_collision_count():
@@ -41,6 +40,6 @@ func _physics_process(delta: float) -> void:
 			player.respawn()
 
 
-#func _on_area_2d_area_entered(area: Area2D) -> void:
-	#if area.is_in_group("Player"):
-		#respawn()
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Player"):
+		respawn()
