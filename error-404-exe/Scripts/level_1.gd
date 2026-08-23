4a2c9ee3aff7a58b0a86d22abaf68ea62d00274d
myles23s-gif <myles23s@gisboyshigh.net>
2026-08-24 10:30:24 +1200

    nav2d regions

--- error-404-exe/Scripts/level_1.gd
+++ error-404-exe/Scripts/level_1.gd
@@ -16,5 +16,17 @@ func _process(delta: float) -> void:
 
 func _on_enemy_spawn_timer_timeout() -> void:
 	var enemy = enemy_prefab.instantiate()
-	enemy.position = Vector2(127,-354)
+	enemy.position = Vector2(3272,-640)
+	add_child(enemy)
+
+
+func _on_enemy_spawn_timer_2_timeout() -> void:
+	var enemy = enemy_prefab.instantiate()
+	enemy.position = Vector2(-752,1680)
+	add_child(enemy)
+
+
+func _on_enemy_spawn_timer_3_timeout() -> void:
+	var enemy = enemy_prefab.instantiate()
+	enemy.position = Vector2(3040,4288)
 	add_child(enemy)
