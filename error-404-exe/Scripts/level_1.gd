2f33027fa3092f5c57f66c2e67f850f8aafac76c
myles23s-gif <myles23s@gisboyshigh.net>
2026-08-25 12:25:51 +1200

    enemy spawn things

--- error-404-exe/Scripts/level_1.gd
+++ error-404-exe/Scripts/level_1.gd
@@ -16,7 +16,7 @@ func _process(delta: float) -> void:
 
 func _on_enemy_spawn_timer_timeout() -> void:
 	var enemy = enemy_prefab.instantiate()
-	enemy.position = Vector2(3272,-640)
+	enemy.position = Vector2(2600,768)
 	add_child(enemy)
 
 
