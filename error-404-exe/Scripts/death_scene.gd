da87704da9b13a266d2310b62aa3baa6c29ee876
myles23s-gif <myles23s@gisboyshigh.net>
2026-09-14 10:21:55 +1200

    finishing touches

--- error-404-exe/Scripts/death_scene.gd
+++ error-404-exe/Scripts/death_scene.gd
@@ -12,5 +12,8 @@ func _process(delta: float) -> void:
 
 
 #changes screen when button is pressed
-func _on_main_menu_button_pressed() -> void:
+
+
+
+func _on_mainmenubutton_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
